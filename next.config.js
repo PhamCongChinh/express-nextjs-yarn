b51af61062f59ee4ea48d901b3f9669ad1091b3a
PhamCongChinh <chinhpham1609@gmail.com>
2021-09-15 12:49:55 +0700

    assetPrefix1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'http://api.thietbihq.com' : 'http://localhost:3000',
+  assetPrefix: isProd ? 'https://api.thietbihq.com' : 'http://localhost:3000',
   compress: false, //Khong nen file
 }
